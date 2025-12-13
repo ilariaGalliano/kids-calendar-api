0fd58498848ee7e38ce6db626bf75112cbaa398c
ilariaGalliano <ilaria.galliano7@gmail.com>
2025-12-13 17:59:01 +0100

    fixed main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const server = express();
-let appInitialized = false;
+let isInitialized = false;
 
 async function bootstrap() {
-  if (!appInitialized) {
-    const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  if (!isInitialized) {
+    const app = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(server),
+    );
+
     app.enableCors();
     await app.init();
-    appInitialized = true;
+
+    isInitialized = true;
   }
-  return server;
 }
 
-export default async function handler(req, res) {
-  const app = await bootstrap();
-  app(req, res);
+export default async function handler(req: Request, res: Response) {
+  await bootstrap();
+  server(req, res);
 }
